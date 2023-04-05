668f45a06f870ea7534a395649f91d045fbae23d
신종흔 <jh.shin@tphealthcare.com>
2023-04-05 10:41:53 +0900

    Prisma 파일DB에서 Postgresql로 변경 (추가됐는지 알수가 없네)

--- server/prisma/migrations/20230404063441_user_table_added/migration.sql
+++ server/prisma/migrations/20230404063441_user_table_added/migration.sql
@@ -1,10 +1,11 @@
 -- CreateTable
 CREATE TABLE "user" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT,
-    "tel" TEXT
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
