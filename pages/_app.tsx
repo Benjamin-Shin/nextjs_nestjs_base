1e1ca9209e02e8fcc744d6b053d1bfaf90e00cd3
신종흔 <jh.shin@tphealthcare.com>
2023-04-05 10:42:22 +0900

    Chakra-ui 프로바이더 및 Layout 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
+import '@/styles/globals.css';
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '@/pages/components/Layout';
+
+import '@/css/tailwind.css';
 
 export default function App({
   Component,
-  pageProps: { session, ...pageProps }
+  pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider resetCSS>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
     </SessionProvider>
-  )
+  );
 }
