fd3f089bd9ef07f6c46f7431f1d3c5e2e5cc66fe
신종흔 <jh.shin@tphealthcare.com>
2023-04-05 10:41:08 +0900

    Next-Auth 수정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,103 +1,51 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import crypto from 'bcrypt';
-
-import { PrismaService } from "@/server/prisma/prisma.service";
-import { user } from '@prisma/client'
-
-// 비밀번호 암호화 함수
-export async function hashPassword(password: string): Promise<string> {
-    const hashedPassword = await crypto.hash(password, 12);
-    return hashedPassword;
-}
-
-// 비밀번호 검증 함수
-export async function verifyPassword(
-    password: string,
-    hashedPassword: string
-): Promise<boolean> {
-    const isValid = await crypto.compare(password, hashedPassword);
-    return isValid
-}
 
 export default NextAuth({
-    providers: [
-        CredentialsProvider({
-            id: 'user-credentials',
-            name: "Credentials",
-            credentials: {
-                nick: { label: "Nick", type: "text", placeholder: "Input login-id" },
-                password: { label: "Password", type: "password" }
-            },
-            authorize: async (credentials, req) => {
-                const user = await fetch(
-                    `${process.env.NEXTAUTH_URL}/api/user/check-credentials`,
-                    {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        accept: "application/json",
-                      },
-                      body: Object.entries(credentials)
-                        .map((e) => e.join("="))
-                        .join("&"),
-                    },
-                  )
-                    .then((res) => res.json())
-                    .catch((err) => {
-                      return null;
-                    });
-          
-                
-                
-                const { nick, password } = credentials!;
-
-
-
-                // 기타 사용자 검증 로직 코드 ...
-                const userInfo = await user.findUnique({
-                    where:{
-                        email
-                    }
-                });
-               
-                if(!userInfo) return null
-
-                const verifyPw = await verifyPassword(password, userInfo.password);
-                if (!verifyPw) return null
-
-                return userInfo
-            }
-        }),
-    ],
-    secret: 'test',
-    pages:{
-        error: '/admin/error',
-        signOut:'/admin'
-    },
-    session: {
-        strategy: 'jwt',
-        maxAge: 24 * 60 * 60,
-        updateAge: 2 * 24 * 60 * 60
-    },
-    callbacks: {
-        async jwt ({token, account}) {
-            if (account) {
-                token.accessToken = account.access_token
-            }
-            return token
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: '사용자ID',
+          type: 'email',
+          placeholder: 'E-Main ID를 입력하세요.',
         },
-        async session({session, token, user}) {
-            return session
-        },
-        async redirect ({url, baseUrl}) {
-            if (url.startsWith("/")) {
-                return `${baseUrl}${url}`
-            }
-            else if ( new URL(url).origin === baseUrl) {
-                return `${baseUrl}`
-            }
-            return baseUrl
-        }
-    }
-})
+        password: { label: '패스워드', type: 'password' },
+      },
+      async authorize(credentials, req) {
+          // Add logic here to look up the user from the credentials supplied
+          if (req.method !== 'POST') {
+              return;
+          }
+
+          // Next.js에서 Nest.js API를 호출
+          const res = await fetch(process.env.NESTAUTH_URL + '/api/user/login', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email: credentials?.email, password: credentials?.password }),
+          });
+          const user = await res.json();
+          console.log("Next-auth login => ", JSON.stringify(user));
+          if (user && user.resultIdx === 0) {
+            return user;
+          } else {
+            return;
+          }
+      }
+    }),
+  ],
+  secret: process.env.SECRET,
+  callbacks: {
+    session({ session, token, user }) {
+      return session; // The return type will match the one returned in `useSession()`
+    },
+  },
+});
